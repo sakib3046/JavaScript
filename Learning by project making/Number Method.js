23561fd9e95c8623851e3f09a505ea70fa487866
Sakib <sakib3046@gmail.com>
2022-10-29 13:33:15 +0600

    Initial commit

---
+++ Learning by project making/Number Method.js
@@ -0,0 +1,18 @@
+console.log("[Int to String]");
+var age = 21;
+
+console.log("Before = " + typeof(age));
+
+ age = toString(age);
+
+console.log("After =  " + typeof(age));
+
+console.log("\n[String to Int]");
+
+var number = "56"
+
+console.log("Before = " + typeof(number));
+
+number = parseInt(number);			//parseFloat available 
+
+console.log("After = " + typeof(number))
